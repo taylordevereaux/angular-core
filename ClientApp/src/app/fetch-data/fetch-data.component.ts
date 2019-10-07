830787b8560ae28e74530ef46f3891ea129a9ed3
Taylor Devereaux <taylordevereaux@live.ca>
2019-10-07 14:46:35 -0300

    Implements fetch data store into fetch data component

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,19 +1,17 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WeatherForecastModel } from '../_models/weather-forecast-model';
 import { SampleDataService } from '../_endpoints/sample-data.service';
+import { FetchDataStore } from './fetch-data.store';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
-export class FetchDataComponent {
-  public forecasts: WeatherForecastModel[];
+export class FetchDataComponent implements OnInit {
+  constructor(public store: FetchDataStore) {}
 
-  constructor(private sampleDataService: SampleDataService) {
-    this.sampleDataService.getWeatherForcasts().subscribe({
-      next: forecasts => (this.forecasts = forecasts),
-      error: errors => console.error(errors)
-    });
+  ngOnInit() {
+    this.store.refreshForecasts();
   }
 }
