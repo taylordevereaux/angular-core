8c3cf03a36ba8dcf64e0d1d070a1e1fb2af8c2a0
Taylor Devereaux <taylordevereaux@live.ca>
2019-12-02 10:21:18 -0400

    Updates Prettier extension settings

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,15 +1,15 @@
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from "./app/app.module";
-import { environment } from "./environments/environment";
-import { hmrBootstrap } from "./hmr";
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 export function getBaseUrl() {
-  return document.getElementsByTagName("base")[0].href;
+  return document.getElementsByTagName('base')[0].href;
 }
 
-const providers = [{ provide: "BASE_URL", useFactory: getBaseUrl, deps: [] }];
+const providers = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
 
 if (environment.production) {
   enableProdMode();
@@ -19,11 +19,11 @@ const bootstrap = () =>
   platformBrowserDynamic(providers).bootstrapModule(AppModule);
 
 if (environment.hmr) {
-  if (module["hot"]) {
+  if (module['hot']) {
     hmrBootstrap(module, bootstrap);
   } else {
-    console.error("HMR is not enabled for webpack-dev-server!");
-    console.log("Are you using the --hmr flag for ng serve?");
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
   }
 } else {
   bootstrap().catch(err => console.log(err));
